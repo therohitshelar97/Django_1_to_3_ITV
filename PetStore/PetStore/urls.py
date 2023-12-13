bd911c77cc718db1894bb40ed96d9bf5b5c106ba
Rohit <rohidasshelar1997@gmail.com>
2023-12-13 18:20:19 +0530

    'petStoreStart'

--- PetStore/PetStore/urls.py
+++ PetStore/PetStore/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project_model project.
+URL configuration for PetStore project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.Forms)
 ]
